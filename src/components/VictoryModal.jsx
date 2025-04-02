e6515f2eed99b9e7d24cde48765f6ec8624ce34e
holinei <holiney2406@gmail.com>
2025-04-03 01:15:10 +0200

    Update v0.39

--- src/components/VictoryModal.jsx
+++ src/components/VictoryModal.jsx
@@ -1,26 +1,57 @@
+// import React from "react";
+// import styles from "./VictoryModal.module.css"; // Стилі для анімації
+// import trophy from "../assets/trophy.png";
+
+// const VictoryModal = ({ winnerName, onExit }) => {
+//   return (
+//     <div className="victory-container">
+//       <div class="container">
+//         <img
+//           className="trophy-icon"
+//           src={trophy}
+//           alt="Trophy"
+//           width="100"
+//           height="100"
+//         />
+
+//         <div class="container__star">
+//           <div class="star-eight"></div>
+//         </div>
+//       </div>
+
+//       <h2 className="victory-message">Переможець: {winnerName}</h2>
+//       <button className="exit-button" onClick={onExit}>
+//         Вийти
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default VictoryModal;
 import React from "react";
-import "VictoryModal.module.css"; // Стилі для анімації
+import styles from "./VictoryModal.module.css";
 import trophy from "../assets/trophy.png";
 
 const VictoryModal = ({ winnerName, onExit }) => {
   return (
-    <div className="victory-container">
-      <div class="container">
+    <div className={styles.victoryContainer}>
+      <div className={styles.container}>
         <img
-          className="trophy-icon"
+          className={styles.trophyIcon}
           src={trophy}
           alt="Trophy"
           width="100"
           height="100"
         />
 
-        <div class="container__star">
-          <div class="star-eight"></div>
+        <div className={styles.containerStar}>
+          <div className={styles.starEight}></div>
         </div>
       </div>
 
-      <h2 className="victory-message">Переможець: {winnerName}</h2>
-      <button className="exit-button" onClick={onExit}>
+      <h2 className={styles.victoryMessage}>Переможець: {winnerName}</h2>
+
+      <button className={styles.exitButton} onClick={onExit}>
         Вийти
       </button>
     </div>
